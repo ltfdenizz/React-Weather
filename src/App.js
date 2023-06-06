3ee7b71acaef245c99eb1ab45ae9c338954c3061
ltfdenizz <ltfdeniz1907@gmail.com>
2023-06-06 16:16:52 +0300

    first added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Wheather from "./wheatherresult";
+import Weather from "./wheatherresult";
+import { useState } from "react";
 
 function App() {
+  const APP_KEY = "306c8214656b4c809b4113340230306";
+  let cityİnput = "";
+  const [wheatherdata, setwheatherdata] = useState([]);
+  function citytext() {
+    document.querySelector("input").addEventListener("input", (e) => {
+      e.preventDefault();
+      cityİnput = e.target.value;
+      console.log(cityİnput);
+    });
+  }
+  async function getdata(value) {
+    if (value === "") return;
+    const data = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no`
+    );
+    const result = await data.json();
+    setwheatherdata(result.forecast.forecastday);
+    console.log(result.forecast.forecastday);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="search">
+        <input type="text" placeholder="Search a city..." onChange={citytext} />
+        <button onClick={() => getdata(cityİnput)}>Search</button>
+      </div>
+      {wheatherdata.map((item) => (
+        <Wheather
+          key={item.key}
+          date={item.date}
+          mintemp={item.day.mintemp_c}
+          maxtemp={item.day.maxtemp_c}
+          condition={item.day.condition.text}
+          icon={item.day.condition.icon}
+        />
+      ))}
     </div>
   );
 }
